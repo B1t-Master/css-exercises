88df2ffd7c89f5300cff6dafbf3642b52df587e3
B1t-Master <isaac.njuguna@strathmore.edu>
2026-04-23 19:47:08 +0300

    Finish drop-down

--- advanced-html-css/animation/03-dropdown-menu/script.js
+++ advanced-html-css/animation/03-dropdown-menu/script.js
@@ -5,11 +5,17 @@ const dropdownMenu = document.querySelector(".dropdown-menu");
 menuTitle.addEventListener("click", (e) => {
   if (e.target === e.currentTarget) {
     dropdownMenu.classList.toggle("visible");
-  }  
-})
+  }
+});
+
+dropdownMenu.addEventListener("animationend", () => {
+  setTimeout(() => {
+    dropdownMenu.classList.toggle("visible");
+  }, 5000);
+});
 
 window.addEventListener("click", (e) => {
   if (!dropdownContainer.contains(e.target)) {
-    dropdownMenu.classList.remove("visible")
+    dropdownMenu.classList.remove("visible");
   }
-})
+});
